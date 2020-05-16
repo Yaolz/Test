6c4e83d575f5a186ccc2bb67de0a653bcaf8a0ed
ZhengYouzhen <1006747577@qq.com>
2020-05-16 18:03:44 +0800

    test

--- SSM/src/test/java/com/gs/service/BaseTest.java
+++ SSM/src/test/java/com/gs/service/BaseTest.java
@@ -7,4 +7,5 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(value = {"classpath:/config/spring-*.xml"})
 public class BaseTest {
+     System.out.println("test1")
 }
